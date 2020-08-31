7d7354e41d2afb13d663e0498094ce1fea1a9871
Adam Hlaváček <git@adamhlavacek.com>
2020-08-31 12:07:33 +0200

    split html and js

---
+++ html.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    if (!window.location.hash) {
+        console.error("No data to parse");
+        return;
+    }
+    const base64 = window.location.hash.substring(1);
+    // noinspection JSCheckFunctionSignatures
+    const compressed = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+
+    const inStream = new LZMA.iStream(compressed);
+    const outStream = LZMA.decompressFile(inStream);
+    const html = outStream.toString();
+
+    document.open();
+    document.write(html);
+    document.close();
+};
