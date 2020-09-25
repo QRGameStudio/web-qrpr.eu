6e03da7da5d6894710bbfe878a04a487331c7f8e
Adam Hlaváček <git@adamhlavacek.com>
2020-09-25 11:59:00 +0200

    feat(html.hml): add super secret edit mode

--- game.js
+++ game.js
@@ -1,3 +1,33 @@
+function superSecretEditMode(gamesStorage) {
+    const defaultCode = `
+<script>
+function openEditWindow() {
+    const w = window.open('editor.html', '_blank');
+    w.onload = () => {
+      const editor = w.document.getElementById('editor');
+      function updateCode() {
+        sessionStorage.setItem('edit', editor.value);
+      }
+      editor.onkeyup = updateCode;
+    };
+    sessionStorage.setItem('edit', '<code>Go to your opened editor tab</code>');
+}
+</script>
+<code>Replace sessionStorage.code to update or open</code>
+<button id="editor-btn" class="btn" onclick="openEditWindow()">Edit window</button>
+    `;
+    sessionStorage.setItem('edit', defaultCode);
+    let editCache = '';
+    setInterval(() => {
+        const currentCode = sessionStorage.getItem('edit');
+        if (currentCode === editCache) {
+            return;
+        }
+        editCache = currentCode;
+        gamesStorage.setGameCode(currentCode).then();
+    }, 1000);
+}
+
 window.onload = async () => {
     const storage = new GStorage("currentGame", true);
     const gamesStorage = new GamesStorage();
@@ -5,11 +35,20 @@ window.onload = async () => {
 
     if (!html) {
         if (window.location.hash) {
-            gamesStorage.loadGame(window.location.hash.substring(1)).then((saved) => {
-                if (saved) {
-                    window.location.reload(true);
-                }
-            });
+            const hashContent = window.location.hash.substring(1);
+            if (hashContent === 'super-secret-edit-mode') {
+                // if you want to try out your game in a live editing environment,
+                // why not just to simply pass the code to the cache
+                superSecretEditMode(gamesStorage);
+            } else {
+                gamesStorage.loadGame(hashContent).then((saved) => {
+                    if (saved) {
+                        window.location.reload(true);
+                    } else {
+                        window.location.replace('index.html');
+                    }
+                });
+            }
         } else {
             window.location.replace('index.html');
         }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const version = 'v2020-09-24::02::';
+const version = 'v2020-09-25::01::';
 const cached = [
     'index.html',
     'index.js',
