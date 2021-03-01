8c502b457eeaa5776fd79de4c43689ac8e5987ec
Adam Hlaváček <git@adamhlavacek.com>
2021-03-01 19:27:16 +0100

    feat(GSound): set sound status to off/fx/all

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
 window.onload = () => {
     const theme = new GTheme();
     const friends = new GFriends();
-    const renderer = new Renderer(document.body, {onlineId: null, theme: theme.get(), connectDisabled: false});
+    const renderer = new Renderer(document.body,
+        {onlineId: null, theme: theme.get(), connectDisabled: false, sound: 'off'});
     const multiplayer = new GMP();
     const modal = new GModal();
 
@@ -38,14 +39,16 @@ window.onload = () => {
     }
 
     renderer.functions.musicEnable = async (enable) => {
-        await GVolume.prototype.create().setMusicEnabled(enable);
+        const value = enable === null ? GSoundStatus.off : enable ? GSoundStatus.music : GSoundStatus.fx;
+        renderer.variables.sound = enable === null ? 'off' : enable ? 'all' : 'fx';
+        GVolume.prototype.get().setStatus(value).then();
         renderer.render();
     }
 
     renderer.functions.addGame = async () => {
         let code = await modal.prompt('Enter game content');
         if (code) {
-            const urlRe =  /^http.*?#(.*)$/.exec(code);
+            const urlRe = /^http.*?#(.*)$/.exec(code);
             if (urlRe) {
                 code = urlRe[1];
             }
@@ -57,6 +60,21 @@ window.onload = () => {
         renderer.variables.onlineId = id;
         renderer.render();
         theme.apply();
+    });
+
+    GVolume.prototype.get().getStatus().then((status) => {
+        switch (status) {
+            case GSoundStatus.fx:
+                renderer.variables.sound = 'fx';
+                break;
+            case GSoundStatus.music:
+                renderer.variables.sound = 'all';
+                break;
+            case GSoundStatus.off:
+                renderer.variables.sound = 'off';
+                break;
+        }
+        renderer.render();
     });
 
     renderer.render();
